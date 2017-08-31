c33d85ac69647234f5855cd86e1a2b90699a3eff
shuhei <shuhei@192.168.11.25>
2017-09-01 00:48:34 +0900

    2回目のコミット

--- src/kadai21/Kadai21.java
+++ src/kadai21/Kadai21.java
@@ -4,13 +4,34 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 
+
+
 public class Kadai21 {
 
 	//ファイル変換用メソッド
 	public String recordToInvoice(String fileName) throws FileNotFoundException {
+
+		String kaigyouCode = "0x0A";
+		int maxKetaForLine = 80;
+		int numberOfLines = 10000000;
+
 		File file = new File(fileName);
 		FileReader filereader = new FileReader(file);
-		return "C:\\Users\\shuhei\\Desktop\\kadai21\\invoice.dat";
+
+
+
+		return "C:\\Users\\shuhei\\Desktop\\kadai21\\output\\invoice.dat";
+	}
+
+	//
+	public String checkKeiyakusyaInfo(String string) {
+		// TODO 自動生成されたメソッド・スタブ
+		return "090-1234-0001";
+	}
+
+	public Object checkKanyuInfo(String string) {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
 	}
 
 }
--- src/kadai21/Kadai21Test.java
+++ src/kadai21/Kadai21Test.java
@@ -12,14 +12,22 @@ public class Kadai21Test {
 	@Test
 	public void RecordToInvoiceTest() throws FileNotFoundException {
 		Kadai21 kd21 = new Kadai21();
-		assertEquals("C:\\Users\\shuhei\\Desktop\\kadai21\\invoice.dat",kd21.recordToInvoice("C:\\Users\\shuhei\\Desktop\\kadai21\\record.log"));
+		assertEquals("C:\\Users\\shuhei\\Desktop\\kadai21\\output\\invoice.dat",kd21.recordToInvoice("C:\\Users\\shuhei\\Desktop\\kadai21\\input\\record.log"));
 	}
 
 
 	@Test
-	public void fileInputTest() throws FileNotFoundException {
-		//Kadai21 kd21 = new Kadai21();
-		//assertTrue(kd21.fileInput("C:\\Users\\shuhei\\Desktop\\kadai21\\record.log"));
+	public void fileReadLineOneTest() throws FileNotFoundException {
+		Kadai21 kd21 = new Kadai21();
+		assertEquals("090-1234-0001",kd21.checkKeiyakusyaInfo("C:\\Users\\shuhei\\Desktop\\kadai21\\record.log"));
 	}
+
+	@Test
+	public void fileReadLineTwoTest() throws FileNotFoundException {
+		Kadai21 kd21 = new Kadai21();
+		assertEquals("090-1234-0001",kd21.checkKanyuInfo("C:\\Users\\shuhei\\Desktop\\kadai21\\record.log"));
+	}
+
+
 
 }
